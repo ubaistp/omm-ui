9cb852f53c84fe5af15386fa13a067429178ce37
Viraj Kokane <virajkokane123@gmail.com>
2020-05-05 08:10:37 +0530

    Added faqs on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './header/header.component';
 import { IndexComponent } from './pages/index/index.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { BorrowComponent } from './pages/borrow/borrow.component';
-import { FaqComponent } from './pages/faq/faq.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
@@ -20,7 +19,6 @@ const appRoutes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'borrow', component: BorrowComponent },
-  { path: 'faq', component: FaqComponent },
 ];
 
 @NgModule({
@@ -29,8 +27,7 @@ const appRoutes: Routes = [
     HeaderComponent,
     IndexComponent,
     AdminComponent,
-    BorrowComponent,
-    FaqComponent
+    BorrowComponent
   ],
   imports: [
     BrowserModule,
