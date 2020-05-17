bfd2d3a5af6ca0620285650592548e27cf6b8eb6
Viraj Kokane <virajkokane123@gmail.com>
2020-05-17 23:37:05 +0530

    onboarding modal changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -97,4 +97,9 @@ export class HeaderComponent implements OnInit, AfterViewInit {
     const last4Digits = address.slice(-4);
     return (start4Digits.padStart(2, '0') + separator.padStart(2, '0') + last4Digits.padStart(2, '0'));
   }
+
+  viewOnboardingModal() {
+    $('#previewModal').modal('show');
+  }
+
 }
