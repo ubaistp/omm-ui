7fe91133c97b9f2c109cfe8afefe7de5f93afd7e
akshay-srivastav <akshay@somish.com>
2020-06-09 13:29:25 +0530

    removed redundant common service

--- src/app/commonData.service.ts
+++ src/app/commonData.service.ts
@@ -4,11 +4,6 @@ import { ReplaySubject } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 import { blockchainConstants } from '../environments/blockchain-constants';
-import * as Comptroller from '../assets/contracts/Comptroller.json';
-import * as PriceOracleProxy from '../assets/contracts/PriceOracleProxy.json';
-import * as CErc20Delegator from '../assets/contracts/CErc20Delegator.json';
-import * as CErc20 from '../assets/contracts/CErc20.json';
-import * as EIP20Interface from '../assets/contracts/EIP20Interface.json';
 
 declare var $: any;
 
@@ -16,11 +11,8 @@ declare var $: any;
   providedIn: 'root',
 })
 export class SharedService {
-  public ethereum: any;
   public web3: any;
   public userAddress: any;
-  public Contracts: any;
-  public contractAddresses: any;
   private proceedApp = new ReplaySubject<any>();
   public proceedApp$ = this.proceedApp.asObservable();
 
@@ -74,7 +66,6 @@ export class SharedService {
       await wcProvider.enable();
       await this.setWalletCookie('wallet-connect');
       this.web3 = new ethers.providers.Web3Provider(wcProvider);
-      // await wcProvider.close()
       await this.setup();
     } catch (error) {
       $('#walletRejectModal').modal('show');
@@ -89,9 +80,8 @@ export class SharedService {
         return;
       }
       window['ethereum'].autoRefreshOnNetworkChange = false;
-      this.ethereum = window['ethereum'];
-      this.web3 = new ethers.providers.Web3Provider(this.ethereum);
-      await this.ethereum.enable();
+      this.web3 = new ethers.providers.Web3Provider(window['ethereum']);
+      await window['ethereum'].enable();
       await this.setWalletCookie('metamask');
       await this.setup();
     } catch (error) {
@@ -135,52 +125,10 @@ export class SharedService {
     this.userAddress = await this.web3.getSigner().getAddress();
     this.toggleWalletConnectionModal('hide');
     this.proceedApp.next(true);
-
-    // const contractAddresses = await this.getContractAddresses();
-    // console.log(this.contractAddresses)
-    // this.initAllContracts(contractAddresses);
-    // console.log(this.userAddress, this.Contracts)
-    // await this.getExchangeRate();
-    // await this.tokenData.forEach(async (token) => {
-    //     this.initToken(token);
-    // });
-    // await this.getEnteredMarkets();
-    // await this.getAccountLiquidity();
-  }
-  private async getContractAddresses() {
-    let contractAddresses = {};
-    this.contractAddresses = {};
-    const network = await this.web3.getNetwork();
-    if (network.name === 'homestead') {
-        contractAddresses = blockchainConstants.mainnet;
-    } else {
-        contractAddresses = blockchainConstants[network.name];
-    }
-    this.contractAddresses = contractAddresses;
-    return contractAddresses;
-  }
-  private async initAllContracts(contractAddresses) {
-    this.Contracts = {};
-    this.Contracts.Comptroller = this.initContract(contractAddresses.Comptroller, Comptroller.abi);
-    this.Contracts.PriceOracleProxy = this.initContract(contractAddresses.PriceOracleProxy, PriceOracleProxy.abi);
-    this.Contracts.cDAI = this.initContract(contractAddresses.cDAI, CErc20Delegator.abi);
-    this.Contracts.cIVTDemo = this.initContract(contractAddresses.cIVTDemo, CErc20.abi);
-    this.Contracts.DAI = this.initContract(contractAddresses.DAI, EIP20Interface.abi);
-    this.Contracts.IVTDemo = this.initContract(contractAddresses.IVTDemo, EIP20Interface.abi);
   }
 
   public getUserAddress() {
     return this.userAddress;
   }
-  public getAllContracts() {
-    // console.log(this.Contracts)
-    return this.Contracts;
-  }
-  // public getUserAddress() {
-  //   return this.userAddress;
-  // }
 
-  private initContract(contractAddress, abi) {
-    return new ethers.Contract(contractAddress, abi, this.web3.getSigner());
-  }
 }
