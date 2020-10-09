6b97857f2726aa91b39adb15e7d0608c9465417e
open-protocols <info@konkrete.io>
2020-10-09 11:46:44 +0530

    CB1 ui fix

--- src/environments/blockchain-constants.ts
+++ src/environments/blockchain-constants.ts
@@ -1,6 +1,5 @@
 export const blockchainConstants = {
-
-  mainnet : {
+  mainnet: {
     Comptroller: '0x959Fb43EF08F415da0AeA39BEEf92D96f41E41b3',
     DynamicInterestRateModel: [
       '0x52948A1BC72a0E4320E84d4A24458314520FAae5',
@@ -12,6 +11,7 @@ export const blockchainConstants = {
       '0x8ff2eC4D76397cCF44b48A3dc4C81Ac2Cba241C8',
       '0xfd6921fE025F612ee0237849671bf8F60FA62F2b',
       '0x0Fc825B3c2333eAAf277cED5CA239EDa82913be5',
+      '0xa46b7FdAEE8fE0BD3a0778C3fCf003F6aA4899F7',
     ],
   },
   ropsten: {
@@ -23,16 +23,6 @@ export const blockchainConstants = {
     DynamicInterestRateModel: ['0xb4CFACe8c502f2F4767d9C6831c00cb0736B0250'],
   },
   kovan: {
-    // Compound
-    // Comptroller: '0x1f5d7f3caac149fe41b8bd62a3673fe6ec0ab73b',
-    // PriceOracleProxy: '0x6998ed7daf969ea0950e01071aceeee54cccbab5',
-
-    // Konkrete
-    // Comptroller: '0x4383939B8105B86490bb4273Cf0aC8F85a0851bd',
-    // PriceOracleProxy: '0x6C01505A174172722Fbde04CBEdDB6d6CE6bb775',
-    // CreditInterestRateModel: '0xaFCB4d6CBd136d4b5fA10c7c255144658314a47F',
-
-    // New Deploy
     Comptroller: '0x0bbBe3Af7a2E31B1C0407Be9D3513d474fc44AB9',
     DynamicInterestRateModel: [
       '0x1237ad42442d98e928fdda93bb1813b0721c59c4',
@@ -44,17 +34,28 @@ export const blockchainConstants = {
       // '0xC922990128E12BE7AFb7424aCFd2CbFcd5a09a0a',
       // '0x35E47C949d064B85F60Aa4c6D66f72F2fEF72362',
     ],
-
-    // test
-    // Comptroller: '0xF1F36dcF5d11f20c0E9Bd6A9207a2493E3C63A99',
-    // PriceOracleProxy: '0x335EFb00A89872d81E43E3dCf5E3d3fA731d911f',
-    // DynamicInterestRateModel: ['0x1237ad42442d98e928fdda93bb1813b0721c59c4'],
-    // cDAI: '0xFFC6340A4D6ED5098Ec2640Af1aB96B9E3BC4559',
-
-    // Comptroller: '0x7e3269B320f838e28B5fda206a57b3052f311883',
-    // PriceOracleProxy: '0xF35B1Fa83A2feEc4Af73f835f02e091596F92878',
-    // DynamicInterestRateModel: ['0x8E609D80bfEB8168E00D3AFe09c140903Bb09FC8'],
-
   },
   infuraID: 'e8f20dc6c35d4e50a38aed78cc360f30',
 };
+
+
+/* test_kovan: {
+  /// Compound
+  // Comptroller: '0x1f5d7f3caac149fe41b8bd62a3673fe6ec0ab73b',
+  // PriceOracleProxy: '0x6998ed7daf969ea0950e01071aceeee54cccbab5',
+
+  /// Konkrete
+  // Comptroller: '0x4383939B8105B86490bb4273Cf0aC8F85a0851bd',
+  // PriceOracleProxy: '0x6C01505A174172722Fbde04CBEdDB6d6CE6bb775',
+  // CreditInterestRateModel: '0xaFCB4d6CBd136d4b5fA10c7c255144658314a47F',
+
+  /// test
+  // Comptroller: '0xF1F36dcF5d11f20c0E9Bd6A9207a2493E3C63A99',
+  // PriceOracleProxy: '0x335EFb00A89872d81E43E3dCf5E3d3fA731d911f',
+  // DynamicInterestRateModel: ['0x1237ad42442d98e928fdda93bb1813b0721c59c4'],
+  // cDAI: '0xFFC6340A4D6ED5098Ec2640Af1aB96B9E3BC4559',
+
+  // Comptroller: '0x7e3269B320f838e28B5fda206a57b3052f311883',
+  // PriceOracleProxy: '0xF35B1Fa83A2feEc4Af73f835f02e091596F92878',
+  // DynamicInterestRateModel: ['0x8E609D80bfEB8168E00D3AFe09c140903Bb09FC8'],
+} */
